f591cbcbb9fdddb2873e1d39625ad3aff2dc5c78
Minal Ahmed <mimalahmed6219@gmail.com>
2024-04-01 20:17:02 +0600

    all design done

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,15 +1,15 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { LandingContent } from "@/components/LandingContent";
+import { LandingHero } from "@/components/LandingHero";
+import { LandingNavbar } from "@/components/LandingNavbar";
 
-export default function LandingPage() {
+const LandingPage = () => {
   return (
-    <div>
-      <p className="text-6xl">Hello AI SaaS (Unprotected)</p>
-      <div>
-        <Link href="/sign-in">
-          <Button>login</Button>
-        </Link>
-      </div>
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
   );
-}
+};
+
+export default LandingPage;
